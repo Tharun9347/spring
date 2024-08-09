d171129693c86574afda14f0a4a6a04801a302d1
Tharun9347 <tharunarishettimsd@gmail.com>
2024-08-09 12:57:09 +0530

    to branch1

---
+++ Javaprograms/src/com/Basics/Heyy.java
@@ -0,0 +1,10 @@
+package com.Basics;
+
+public class Heyy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("dgdgf");
+	}
+
+}
---
+++ Javaprograms/src/com/Basics/Primee.java
@@ -0,0 +1,25 @@
+package com.Basics;
+
+public class Primee {
+
+	public static void main(String[] args) {
+		//int i=1,n=100;
+		for(int i=1;i<=100;i++)
+		{
+			int count=0;
+			for(int j=1;j<=i;j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==2)
+			{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
